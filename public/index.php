9ac80d87ec3d368457e0f947d847c6e8509bc0d1
Blackpa77 <h1101241029@student.untan.ac.id>
2025-11-06 10:02:04 +0700

    sementara langkah 1

--- public/index.php
+++ public/index.php
@@ -1,5 +1,14 @@
 <?php
 
+echo "<pre>";
+echo "MYSQLHOST=" . getenv('MYSQLHOST') . "\n";
+echo "MYSQLUSER=" . getenv('MYSQLUSER') . "\n";
+echo "MYSQLDATABASE=" . getenv('MYSQLDATABASE') . "\n";
+echo "MYSQLPORT=" . getenv('MYSQLPORT') . "\n";
+echo "</pre>";
+exit;
+
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 require_once __DIR__ . '/../app/core/Database.php';
